5bb42925429666c510aab40240cf34647ac21ea3
Sang-Jin <lee3jjang@gmail.com>
2021-07-15 21:25:46 +0900

    dfdf

--- hotreload/main.py
+++ hotreload/main.py
@@ -1,28 +1,41 @@
-from kaki.app import App
-from kivymd.app import MDApp
-
 from kivy.lang import Builder
-from kivy.clock import Clock
-from kivy.core.window import Window
+from kivymd.app import MDApp
+from datetime import datetime
 from kivy.factory import Factory
-Window.size = (500, 750)
-Window.left, Window.top = 1350, -900
 
-class MDLive(App, MDApp):
+KV = '''
+#:import KivyLexer kivy.extras.highlight.KivyLexer
+#:import HotReloadViewer kivymd.utils.hot_reload_viewer.HotReloadViewer
 
-    CLASSES = {
-        'ScreenUI': "liveapp.screen_ui",
-    }
+BoxLayout:
 
-    AUTORELOADER_PATHS = [
-        (".", {"recursive": True})
-    ]
+    CodeInput:
+        lexer: KivyLexer()
+        style_name: "native"
+        on_text: app.update_kv_file(self.text)
+        size_hint_x: .6
 
-    def build_app(self, *args):
-        print("Inside Build App Auto Reload 2")
-        return Factory.ScreenUI()
+    HotReloadViewer:
+        size_hint_x: .4
+        path: app.path_to_kv_file
+        errors: True
+        errors_text_color: 1, 1, 0, 1
+        errors_background_color: app.theme_cls.bg_dark
+'''
 
-    
+class Example(MDApp):
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    path_to_kv_file = f"hotreload/main_{now}.kv"
 
-if __name__ == '__main__':
-    MDLive().run()
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        # return Builder.load_string(KV)
+        Builder.load_file('main.kv')
+        return Factory.MainScreen()
+
+    def update_kv_file(self, text):
+        with open(self.path_to_kv_file, "w") as kv_file:
+            kv_file.write(text)
+
+
+Example().run()
