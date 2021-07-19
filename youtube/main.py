ecc48b82be64401b40747e298b4b14f82d441a64
Sang-Jin <lee3jjang@gmail.com>
2021-07-19 22:32:41 +0900

    aass

---
+++ youtube/main.py
@@ -0,0 +1,36 @@
+from kivystudio import KivyStudio
+from kivymd.uix.screen import MDScreen
+from pathlib import Path
+from kivy.clock import Clock
+import os
+import pytube
+from kivymd.toast import toast
+
+
+class MainScreen(MDScreen):
+
+    image_loaded = False
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def set_assets(self, thumbnail, title):
+        self.ids.thumbnail.source = thumbnail
+        self.ids.title.text = title
+
+    def download_video(self, url):
+        yt = pytube.YouTube(url)
+        self.set_assets(yt.thumbnail_url, yt.title)
+        self.image_loaded = True
+        Clock.schedule_once(lambda x: self.get_video(yt.streams.first()), 4)
+
+    def get_video(self, stream):
+        if self.image_loaded == True:
+            stream.download()
+        toast("Video is downloading...")
+
+
+
+if __name__ == '__main__':
+    studio = KivyStudio(str(Path(os.path.dirname(__file__)) / 'main.kv'))
+    studio.run()
